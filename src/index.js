2d0277850dd637aac1bdf53bfd6d242a787d399c
LuoHuacheng <154224099@qq.com>
2018-09-13 17:04:36 +0800

    移除项目不需要的组件

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import Affix from './components/affix';
-import Alert from './components/alert';
-import Anchor from './components/anchor';
-import AnchorLink from './components/anchor-link';
-import AutoComplete from './components/auto-complete';
+// import Affix from './components/affix';
+// import Alert from './components/alert';
+// import Anchor from './components/anchor';
+// import AnchorLink from './components/anchor-link';
+// import AutoComplete from './components/auto-complete';
 import Avatar from './components/avatar';
-import BackTop from './components/back-top';
+// import BackTop from './components/back-top';
 import Badge from './components/badge';
 import Breadcrumb from './components/breadcrumb';
 import Button from './components/button';
 import Card from './components/card';
-import Carousel from './components/carousel';
-import Cascader from './components/cascader';
+// import Carousel from './components/carousel';
+// import Cascader from './components/cascader';
 import Cell from './components/cell';
 import Checkbox from './components/checkbox';
-import Circle from './components/circle';
+// import Circle from './components/circle';
 import Collapse from './components/collapse';
-import ColorPicker from './components/color-picker';
+// import ColorPicker from './components/color-picker';
 import Content from './components/content';
-import DatePicker from './components/date-picker';
+// import DatePicker from './components/date-picker';
 import Divider from './components/divider';
 import Dropdown from './components/dropdown';
 import Footer from './components/footer';
@@ -26,8 +26,8 @@ import Header from './components/header';
 import Icon from './components/icon';
 import Input from './components/input';
 import InputNumber from './components/input-number';
-import Scroll from './components/scroll';
-import Split from './components/split';
+// import Scroll from './components/scroll';
+// import Split from './components/split';
 import Layout from './components/layout';
 import LoadingBar from './components/loading-bar';
 import Menu from './components/menu';
@@ -38,52 +38,52 @@ import Page from './components/page';
 import Poptip from './components/poptip';
 import Progress from './components/progress';
 import Radio from './components/radio';
-import Rate from './components/rate';
-import Sider from './components/sider';
-import Slider from './components/slider';
+// import Rate from './components/rate';
+// import Sider from './components/sider';
+// import Slider from './components/slider';
 import Spin from './components/spin';
 import Steps from './components/steps';
 import Switch from './components/switch';
 import Table from './components/table';
 import Tabs from './components/tabs';
 import Tag from './components/tag';
-import Time from './components/time';
-import Timeline from './components/timeline';
-import TimePicker from './components/time-picker';
+// import Time from './components/time';
+// import Timeline from './components/timeline';
+// import TimePicker from './components/time-picker';
 import Tooltip from './components/tooltip';
 import Transfer from './components/transfer';
-import Tree from './components/tree';
-import Upload from './components/upload';
+// import Tree from './components/tree';
+// import Upload from './components/upload';
 import {Row, Col} from './components/grid';
 import {Select, Option, OptionGroup} from './components/select';
 import locale from './locale/index';
 
 const components = {
-    Affix,
-    Alert,
-    Anchor,
-    AnchorLink,
-    AutoComplete,
+    // Affix,
+    // Alert,
+    // Anchor,
+    // AnchorLink,
+    // AutoComplete,
     Avatar,
-    BackTop,
+    // BackTop,
     Badge,
     Breadcrumb,
     BreadcrumbItem: Breadcrumb.Item,
     Button,
     ButtonGroup: Button.Group,
     Card,
-    Carousel,
-    CarouselItem: Carousel.Item,
-    Cascader,
+    // Carousel,
+    // CarouselItem: Carousel.Item,
+    // Cascader,
     Cell,
     CellGroup: Cell.Group,
     Checkbox,
     CheckboxGroup: Checkbox.Group,
     Col,
     Collapse,
-    ColorPicker,
+    // ColorPicker,
     Content: Content,
-    DatePicker,
+    // DatePicker,
     Divider,
     Dropdown,
     DropdownItem: Dropdown.Item,
@@ -95,10 +95,10 @@ const components = {
     Icon,
     Input,
     InputNumber,
-    Scroll,
-    Sider: Sider,
-    Split,
-    Submenu: Menu.Sub,
+    // Scroll,
+    // Sider: Sider,
+    // Split,
+    // Submenu: Menu.Sub,
     Layout: Layout,
     LoadingBar,
     Menu,
@@ -110,15 +110,15 @@ const components = {
     Option: Option,
     OptionGroup,
     Page,
-    Panel: Collapse.Panel,
+    // Panel: Collapse.Panel,
     Poptip,
     Progress,
     Radio,
     RadioGroup: Radio.Group,
-    Rate,
+    // Rate,
     Row,
     Select,
-    Slider,
+    // Slider,
     Spin,
     Step: Steps.Step,
     Steps,
@@ -126,20 +126,20 @@ const components = {
     Tabs: Tabs,
     TabPane: Tabs.Pane,
     Tag,
-    Time,
-    Timeline,
-    TimelineItem: Timeline.Item,
-    TimePicker,
+    // Time,
+    // Timeline,
+    // TimelineItem: Timeline.Item,
+    // TimePicker,
     Tooltip,
     Transfer,
-    Tree,
-    Upload
+    // Tree,
+    // Upload
 };
 
 const iview = {
     ...components,
     iButton: Button,
-    iCircle: Circle,
+    // iCircle: Circle,
     iCol: Col,
     iContent: Content,
     iForm: Form,
@@ -148,11 +148,11 @@ const iview = {
     iInput: Input,
     iMenu: Menu,
     iOption: Option,
-    iProgress: Progress,
+    // iProgress: Progress,
     iSelect: Select,
     iSwitch: Switch,
     iTable: Table,
-    iTime: Time
+    // iTime: Time
 };
 
 const install = function(Vue, opts = {}) {
@@ -186,7 +186,7 @@ const API = {
     locale: locale.use,
     i18n: locale.i18n,
     install,
-    Circle,
+    // Circle,
     Switch,
     ...components
 };
